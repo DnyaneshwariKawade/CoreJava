a2822eee344660a1b224b2518ef4b3ea322a9df9
DnyaneshwariKawade <dnyaneshwarikiran@gmail.com>
2024-04-18 19:32:56 +0530

    file name updates

--- src/main/java/com/core/common/java8/FilterAndMapTest.java
+++ src/main/java/com/core/common/java8/FilterAndMapTest.java
@@ -4,7 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class FilerAndMapTest {
+public class FilterAndMapTest {
+
 
     public static void main(String[] args) {
         Stream<Integer> nums = Stream.of(1, 2, 3, 4, 5);
